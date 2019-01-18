cyberpay/galilel

commit 3b4955969e84613c963190430db3823cd1b05a1e

Dynamic zPoS v2:
In Galilel v3.1.0 it was a technology preview feature because we still
use zGALI denominator 1, so it means in Phase 9 we need to generate
6 * 1 zGALI which is very CPU intensive and probability to generate an
orphan block is higher since a public coin staker can solve the block
later but distribute it to the chain earlier.

In Galilel v3.1.1 we auto resolve best denomination structure. In our
case it means the following:

- v3.1.0: 6 * 1 zGALI (6 mints, each requires CPU time)
- v3.1.1: 1 * 1 zGALI + 1 * 5 zGALI (2 mints, lower probability to
generate an orphan block)

The biggest benefit for blockchain developers is that it makes coin
forks with zPoS much easier as auto resolve of best denomination
structure will even work for coins which generate very high amounts
per block.

src/stakeinput.cpp
@@ -140,11 +140,27 @@ bool CZGaliStake::CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount n
     CAmount blockValue = GetBlockValue(chainActive.Height() + 1);
     CAmount masternodePayment = GetMasternodePayment(chainActive.Height() + 1, blockValue, 0, true);
 
-    for (unsigned int i = 0; i < ((blockValue - masternodePayment) / COIN); i++) {
+    // dynamic zPoS v2 which determines the closest denomation to the current reward
+    CAmount nValue = blockValue - masternodePayment;
+    while (true) {
+
+        // nothing left for minting
+        if (nValue < 1 * COIN)
+            break;
+
+        // mint a coin with the closest denomination to what is being requested
+        libzerocoin::CoinDenomination denomination = libzerocoin::AmountToClosestDenomination(nValue, nValue);
+        if (denomination == libzerocoin::ZQ_ERROR)
+            return error("%s: failed to create closest denomination", __func__);
+
+        libzerocoin::ZerocoinDenominationToAmount(denomination);
+
         CTxOut out;
         CDeterministicMint dMintReward;
-        if (!pwallet->CreateZGALIOutPut(libzerocoin::CoinDenomination::ZQ_ONE, out, dMintReward))
+
+        if (!pwallet->CreateZGALIOutPut(denomination, out, dMintReward))
             return error("%s: failed to create zGALI output", __func__);
+
         vout.emplace_back(out);
 
         if (!pwallet->DatabaseMint(dMintReward))

commit 7e7f20f3649292d519891ab02dbf94ee5eff121c

Fixed Dynamic zPoS v2 bug which generate only first closest denomination, introduced with commit 3b4955969e84613c963190430db3823cd1b05a1e

src/stakeinput.cpp
@@ -153,6 +153,9 @@ bool CZGaliStake::CreateTxOuts(CWallet* pwallet, vector<CTxOut>& vout, CAmount n
         if (denomination == libzerocoin::ZQ_ERROR)
             return error("%s: failed to create closest denomination", __func__);
 
+        // recalculate remaining value
+        nValue = nValue * COIN;
+
         libzerocoin::ZerocoinDenominationToAmount(denomination);
 
         CTxOut out;