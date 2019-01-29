f1c373c7f66e4582ba0ac6cf170e0e3ab23fb9ce
Maik Broemme <mbroemme@libmpq.org>
2019-01-29 14:41:38 +0200

    [Fix] Stake spent amplification attack fix for commit 1fe39e9ca036f915f27322dfb575da78c0163984
    Commit 1fe39e9ca036f915f27322dfb575da78c0163984 fixed spend stake
    amplification attack but has two issues:
    
    1. It does no proper iteration through mapStakeSpent and erase of
       old entries will crash the wallet.
    
    2. It doesn't use locks and crashes certain operations like chain
       reindexing.
    
    Both fixed with this commit and with both commits the fix is working.

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,7 @@ using namespace libzerocoin;
  */
 
 CCriticalSection cs_main;
+CCriticalSection cs_mapstake;
 
 BlockMap mapBlockIndex;
 map<uint256, uint256> mapProofOfStake;
@@ -2207,8 +2208,12 @@ bool DisconnectBlock(CBlock& block, CValidationState& state, CBlockIndex* pindex
                     coins->vout.resize(out.n + 1);
                 coins->vout[out.n] = undo.txout;
 
-                // erase the spent input
-                mapStakeSpent.erase(out);
+                {
+                    LOCK(cs_mapstake);
+
+                    // erase the spent input
+                    mapStakeSpent.erase(out);
+                }
             }
         }
     }
@@ -2784,19 +2789,26 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         if (!pblocktree->WriteTxIndex(vPos))
             return state.Abort("Failed to write transaction index");
 
-    // add new entries
-    for (const CTransaction tx: block.vtx) {
-        if (tx.IsCoinBase() || tx.IsZerocoinSpend())
-            continue;
-        for (const CTxIn in: tx.vin) {
-            mapStakeSpent.insert(std::make_pair(in.prevout, pindex->nHeight));
-        }
-    }
+    {
+        LOCK(cs_mapstake);
 
-    // delete old entries
-    for (auto it = mapStakeSpent.begin(); it != mapStakeSpent.end(); ++it) {
-        if (it->second < pindex->nHeight - Params().MaxReorganizationDepth()) {
-            mapStakeSpent.erase(it->first);
+        // add new entries
+        for (const CTransaction tx: block.vtx) {
+            if (tx.IsCoinBase() || tx.IsZerocoinSpend())
+                continue;
+            for (const CTxIn in: tx.vin) {
+                mapStakeSpent.insert(std::make_pair(in.prevout, pindex->nHeight));
+            }
+        }
+
+        // delete old entries
+        for (auto it = mapStakeSpent.begin(); it != mapStakeSpent.end();) {
+            if (it->second < pindex->nHeight - Params().MaxReorganizationDepth()) {
+                it = mapStakeSpent.erase(it);
+            }
+            else {
+                it++;
+            }
         }
     }
 
@@ -4075,6 +4087,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         CCoinsViewCache coins(pcoinsTip);
 
         if (!coins.HaveInputs(block.vtx[1])) {
+            LOCK(cs_mapstake);
 
             // the inputs are spent at the chain tip so we should look at the recently spent outputs
             for (CTxIn in : block.vtx[1].vin) {
