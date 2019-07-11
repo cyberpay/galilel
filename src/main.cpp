efe0af2da3868915e918e39d902f41d7cba3ceb9
Maik Broemme <mbroemme@libmpq.org>
2019-07-11 10:41:21 +0200

    Merged https://github.com/PIVX-Project/PIVX/pull/925 [Consensus] Time checks

--- src/main.cpp
+++ src/main.cpp
@@ -4232,31 +4232,6 @@ bool AcceptBlockHeader(const CBlock& block, CValidationState& state, CBlockIndex
     return true;
 }
 
-bool ContextualCheckZerocoinStake(int nHeight, CStakeInput* stake)
-{
-    if (nHeight < Params().Zerocoin_Block_V2_Start())
-        return error("%s: zGALI stake block is less than allowed start height", __func__);
-
-    if (CZGaliStake* zGALI = dynamic_cast<CZGaliStake*>(stake)) {
-        CBlockIndex* pindexFrom = zGALI->GetIndexFrom();
-        if (!pindexFrom)
-            return error("%s: failed to get index associated with zGALI stake checksum", __func__);
-
-        if (chainActive.Height() - pindexFrom->nHeight < Params().Zerocoin_RequiredStakeDepth())
-            return error("%s: zGALI stake does not have required confirmation depth. Current height %d,  stakeInput height %d.", __func__, chainActive.Height(), pindexFrom->nHeight);
-
-        //The checksum needs to be the exact checksum from 240 blocks ago
-        uint256 nCheckpoint = chainActive[nHeight - Params().Zerocoin_RequiredStakeDepth()]->nAccumulatorCheckpoint;
-        uint32_t nChecksum = ParseChecksum(nCheckpoint, libzerocoin::AmountToZerocoinDenomination(zGALI->GetValue()));
-        if (nChecksum != zGALI->GetChecksum())
-            return error("%s: accumulator checksum is different than the block %d blocks previous. stake=%d block240=%d", __func__, Params().Zerocoin_RequiredStakeDepth(), zGALI->GetChecksum(), nChecksum);
-    } else {
-        return error("%s: dynamic_cast of stake ptr failed", __func__);
-    }
-
-    return true;
-}
-
 bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, CDiskBlockPos* dbp, bool fAlreadyCheckedBlock)
 {
     AssertLockHeld(cs_main);
@@ -4295,14 +4270,11 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
         uint256 hashProofOfStake = 0;
         unique_ptr<CStakeInput> stake;
 
-        if (!CheckProofOfStake(block, hashProofOfStake, stake))
+        if (!CheckProofOfStake(block, hashProofOfStake, stake, pindexPrev->nHeight))
             return state.DoS(100, error("%s: proof of stake check failed", __func__));
 
         if (!stake)
             return error("%s: null stake ptr", __func__);
-
-        if (stake->IsZGALI() && !ContextualCheckZerocoinStake(pindexPrev->nHeight, stake.get()))
-            return state.DoS(100, error("%s: staked zGALI fails context checks", __func__));
 
         uint256 hash = block.GetHash();
         if(!mapProofOfStake.count(hash)) // add to mapProofOfStake
